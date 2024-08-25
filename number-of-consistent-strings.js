12f5d78f1dd3c5c9a20fac6f168cf4dfe30489e2
Saraha <sah110_@outlook.com>
2024-08-24 21:22:31 -0400

    Create number-of-consistent-strings.js

---
+++ number-of-consistent-strings.js
@@ -0,0 +1,12 @@
+//You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+// Return the number of consistent strings in the array words.
+
+var countConsistentStrings = function (allowed, words) {
+    const allowedSet = new Set(allowed);
+
+    const consistentWords = words.filter(word => {
+        return [...word].every(char => allowedSet.has(char));
+    });
+
+    return consistentWords.length;
+};

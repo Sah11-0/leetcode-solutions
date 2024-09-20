bbb580b4430447caecc8724fbf94fe8fd38fa5e3
Saraha <sah110_@outlook.com>
2024-09-19 20:30:34 -0400

    Create convert-sorted-array-bst.js

---
+++ convert-sorted-array-bst.js
@@ -0,0 +1,14 @@
+//Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+var sortedArrayToBST = function (nums) {
+    if (nums.length === 0) return null;
+
+    const mid = Math.floor((nums.legnth) / 2);
+    const root = new TreeNode(nums[mid]);
+
+    root.left = sortedArrayToBST(nums.slice(0, mid));
+    root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+    return root;
+
+};

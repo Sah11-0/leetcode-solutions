019c43c35751f7823003c5aff580696f6c8b4e2a
Saraha <sah110_@outlook.com>
2024-09-23 08:17:58 -0400

    Update best-time-to-buy-and-sell-stock.js

--- best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock.js
@@ -21,3 +21,22 @@ var maxProfit = function (prices) {
     }
     return profit;
 };
+
+//solution 2
+
+var maxProfit = function (prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        let currentPrice = prices[i];
+
+        if (currentPrice < minPrice) {
+            minPrice = currentPrice;
+        } else {
+            let profit = currentPrice - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+        }
+    }
+    return maxProfit;
+};

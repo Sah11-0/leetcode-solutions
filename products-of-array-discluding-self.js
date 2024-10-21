4e85ae7ef3df36135c2234d00dd017881f87a0f0
Saraha <sah110_@outlook.com>
2024-10-20 21:00:39 -0400

    Create products-of-array-discluding-self.js

---
+++ products-of-array-discluding-self.js
@@ -0,0 +1,24 @@
+// Given an integer array nums, return an array output where output[i] is the product of all the elements of nums except nums[i].
+// Each product is guaranteed to fit in a 32-bit integer.
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const n = nums.length;
+        const res = new Array(n).fill(1);
+
+        for (let i = 1; i < n; i++) {
+            res[i] = res[i - 1] * nums[i - 1];
+        }
+
+        let postfix = 1;
+        for (let i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+        return res;
+    }
+}

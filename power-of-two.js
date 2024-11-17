c96f61fb8b98f2e9229699385ea44adc198cac9e
Saraha <sah110_@outlook.com>
2024-11-17 16:11:26 -0500

    Update power-of-two.js

--- power-of-two.js
+++ power-of-two.js
@@ -3,3 +3,13 @@
 var isPowerOfTwo = function (n) {
     return n > 0 && (n & (n - 1)) === 0;
 };
+
+//iterative
+function isPowerOfTwoIterative(n) {
+    if (n <= 0) return false;
+    while (n > 1) {
+        if (n % 2 !== 0) return false;
+        n = n / 2;
+    }
+    return true;
+}

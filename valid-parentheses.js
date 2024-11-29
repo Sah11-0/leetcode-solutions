6d9715e56bdf1a6e4a5536ebc3f32509ef5b5150
Saraha <sah110_@outlook.com>
2024-11-29 18:37:28 -0500

    Update valid-parentheses.js

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -27,3 +27,27 @@ var isValid = function (s) {
     return stack.length === 0;
 
 };
+
+//solution 2
+
+function isValid(s) {
+    const stack = [];
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    };
+
+    for (let char of s) {
+        if (char in map) {
+            const top = stack.length > 0 ? stack.pop() : '#';
+            if (top !== map[char]) {
+                return false;
+            }
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}

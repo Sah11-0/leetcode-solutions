c106fafb078c5d078a3edbd2cbd6308a82c51364
Saraha <sah110_@outlook.com>
2024-12-17 18:53:50 -0500

    Update find-all-disappeared-nums.py

--- find-all-disappeared-nums.py
+++ find-all-disappeared-nums.py
@@ -11,3 +11,18 @@ class Solution:
                 ret.append(i)
 
         return ret
+    
+# Solution 2
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            temp = abs(nums[i]) - 1
+            if nums[temp] > 0:
+                nums[temp] *= -1
+
+        res = []
+        for i, n in enumerate(nums):
+            if n > 0:
+                res.append(i + 1)
+
+        return res

8b660863e564e63d7d832cf7df897fd1bff18588
Saraha <sah110_@outlook.com>
2025-01-22 22:34:58 -0500

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,7 +1,16 @@
 //Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
 
 async function sleep(millis) {
-    return new Promise(resolve =>{
+    return new Promise(resolve => {
         setTimeout(resolve, millis);
+    });
+}
+
+//solution 2
+async function sleep(millis) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve('resolved');
+        }, millis);
     });
 }
